d943866d75c58fd8764fcda685a5f72154f71757
Hiroaki Imai <hiro.1114.aki@gmail.com>
2020-03-24 01:54:14 +0900

    Added custom files.

---
+++ frontend/pages/_app.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import App, { AppContext } from 'next/app';
+import 'ress'
+
+class CustomApp extends App {
+  static async getInitialProps({ Component, ctx }: AppContext) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    return <Component {...pageProps} />;
+  }
+}
+
+export default CustomApp
---
+++ frontend/pages/_document.tsx
@@ -0,0 +1,37 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+interface Props {
+  styleTags: any;
+}
+
+class CustomDocument extends Document<Props> {
+  static async getInitialProps(context: DocumentContext) {
+    const renderPage = context.renderPage;
+    // Step 1: Create an instance of ServerStyleSheet
+    const sheet = new ServerStyleSheet();
+    // Step 2: Retrieve styles from components in the page
+    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
+    // Step 3: Extract the styles as <style> tags
+    const styleTags = sheet.getStyleElement();
+    // Step 4: Pass styleTags as a prop
+    return { ...page, styleTags };
+  }
+
+  render() {
+    return (
+      <Html lang="js">
+        <Head>
+          <meta charSet="utf-8" />
+          {this.props.styleTags}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default CustomDocument;
